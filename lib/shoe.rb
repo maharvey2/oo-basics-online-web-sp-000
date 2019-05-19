82f1fc11a72c39885ee2b909fd5442a170202c25
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-19 21:14:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,8 +7,8 @@ def initialize(brand)
   @brand = brand
 end
 
-def cobble
-  @condition = "new"
-  puts "Your shoe is as good as new!"
-end
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
 end
